5722444b1cf1e5bc805e345a1c026e2a7d2fa642
roluochKe <rasholuoch@gmail.com>
2021-11-16 15:26:15 +0300

    forms and component

--- src/App.js
+++ src/App.js
@@ -5,13 +5,20 @@ import './App.css'
 
 import HomePage from './pages/homepage/homepage.component.jsx'
 import ShopPage from './pages/shop/shop.component'
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
+
+import Header from './components/header/header.component'
 
 const App = () => {
   return (
-    <Routes>
-      <Route exact path='/' element={<HomePage />} />
-      <Route path='/shop' element={<ShopPage />} />
-    </Routes>
+    <div>
+      <Header />
+      <Routes>
+        <Route exact path='/' element={<HomePage />} />
+        <Route path='/shop' element={<ShopPage />} />
+        <Route path='/signin' element={<SignInAndSignUpPage />} />
+      </Routes>
+    </div>
   )
 }
 
