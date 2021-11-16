6dc79677f26f43646eb0a7f134cfa4da93b49434
roluochKe <rasholuoch@gmail.com>
2021-11-16 14:45:02 +0300

    collection preview

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import { Routes, Route } from 'react-router-dom'
 import './App.css'
 
 import HomePage from './pages/homepage/homepage.component.jsx'
+import ShopPage from './pages/shop/shop.component'
 
 const App = () => {
   return (
     <Routes>
       <Route exact path='/' element={<HomePage />} />
+      <Route path='/shop' element={<ShopPage />} />
     </Routes>
   )
 }
