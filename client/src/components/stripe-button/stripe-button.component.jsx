b69198264157c8c098803f9617e57d0edc50a8af
roluochKe <rasholuoch@gmail.com>
2021-11-18 20:39:38 +0300

    payment server

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import StripeCheckout from 'react-stripe-checkout'
+import axios from 'axios'
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100
@@ -7,8 +8,23 @@ const StripeCheckoutButton = ({ price }) => {
     'pk_test_51JMZeHC6su9OEK8y56jiPWiVKeiQcke6Z8XuTFaUhz8CCTGHAt69X0E0LrRoAWvKjJC1UxBjiNWoFFtZVrrjOS2G00MG96Je2M'
 
   const onToken = (token) => {
-    console.log(token)
-    alert('Payment Succesful!')
+    axios({
+      url: 'payment',
+      method: 'post',
+      data: {
+        amount: priceForStripe,
+        token: token,
+      },
+    })
+      .then((response) => {
+        alert('succesful payment')
+      })
+      .catch((error) => {
+        console.log('Payment Error: ', error)
+        alert(
+          'There was an issue with your payment! Please make sure you use the provided credit card.'
+        )
+      })
   }
 
   return (
