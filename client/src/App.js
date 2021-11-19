c633ae7d688fad4af36fefbf7df78385a15456fb
roluochKe <rasholuoch@gmail.com>
2021-11-19 12:54:05 +0300

    react lazy, suspense, memo and error boundary

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,23 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, lazy, Suspense } from 'react'
 import { Routes, Route, Navigate } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { createStructuredSelector } from 'reselect'
 
-import HomePage from './pages/homepage/homepage.component'
-import ShopPage from './pages/shop/shop.component'
-import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
-import CheckoutPage from './pages/checkout/checkout.component'
-
 import Header from './components/header/header.component'
+import Spinner from './components/spinner/spinner.component'
+import ErrorBoundary from './components/error-boundary/error-boundary.component'
 
 import { GlobalStyle } from './global.styles'
 
 import { selectCurrentUser } from './redux/user/user.selectors'
 import { checkUserSession } from './redux/user/user.actions'
+
+const HomePage = lazy(() => import('./pages/homepage/homepage.component'))
+const ShopPage = lazy(() => import('./pages/shop/shop.component'))
+const SignInAndSignUpPage = lazy(() =>
+  import('./pages/sign-in-and-sign-up/sign-in-and-sign-up.component')
+)
+const CheckoutPage = lazy(() => import('./pages/checkout/checkout.component'))
 
 const App = ({ checkUserSession, currentUser }) => {
   useEffect(() => {
@@ -25,14 +29,20 @@ const App = ({ checkUserSession, currentUser }) => {
       <GlobalStyle />
       <Header />
       <Routes>
-        <Route exact path='/' element={<HomePage />} />
-        <Route path='/shop/*' element={<ShopPage />} />
-        <Route exact path='/checkout' element={<CheckoutPage />} />
-        <Route
-          exact
-          path='/signin'
-          element={currentUser ? <Navigate to='/' /> : <SignInAndSignUpPage />}
-        />
+        <ErrorBoundary>
+          <Suspense fallback={<Spinner />}>
+            <Route exact path='/' element={<HomePage />} />
+            <Route path='/shop/*' element={<ShopPage />} />
+            <Route exact path='/checkout' element={<CheckoutPage />} />
+            <Route
+              exact
+              path='/signin'
+              element={
+                currentUser ? <Navigate to='/' /> : <SignInAndSignUpPage />
+              }
+            />
+          </Suspense>
+        </ErrorBoundary>
       </Routes>
     </div>
   )
--- client/src/components/spinner/spinner.styles.jsx
+++ client/src/components/spinner/spinner.styles.jsx
@@ -1,12 +1,14 @@
-.spinner-overlay {
+import styled from 'styled-components';
+
+export const SpinnerOverlay = styled.div`
   height: 60vh;
   width: 100%;
   display: flex;
   justify-content: center;
   align-items: center;
-}
+`;
 
-.spinner-container {
+export const SpinnerContainer = styled.div`
   display: inline-block;
   width: 50px;
   height: 50px;
@@ -15,7 +17,6 @@
   border-top-color: #636767;
   animation: spin 1s ease-in-out infinite;
   -webkit-animation: spin 1s ease-in-out infinite;
-
   @keyframes spin {
     to {
       -webkit-transform: rotate(360deg);
@@ -26,4 +27,4 @@
       -webkit-transform: rotate(360deg);
     }
   }
-}
+`;
